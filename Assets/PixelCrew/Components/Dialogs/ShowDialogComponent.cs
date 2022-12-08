1d3f9a63b4c4a708a7fb09cce8300fada126d20a
AlexB ID <butenko.92@mail.ru>
2022-12-09 00:22:17 +0300

    Intermeadiate results HW18

---
+++ Assets/PixelCrew/Components/Dialogs/ShowDialogComponent.cs
@@ -0,0 +1,53 @@
+﻿using Assets.PixelCrew.Model.Data;
+using Assets.PixelCrew.Model.Definitions;
+using Assets.PixelCrew.UI.Hud.Dialogs;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.PixelCrew.Components.Dialogs
+{
+    public class ShowDialogComponent : MonoBehaviour
+    {
+        [SerializeField] private Mode _mode;
+        [SerializeField] private DialogData _bound;
+        [SerializeField] private DialogDef _external;
+
+        private DialogBoxController _dialogBox;
+        public void Show()
+        {
+            if (_dialogBox == null)
+                _dialogBox = FindObjectOfType<DialogBoxController>();
+
+            _dialogBox.ShowDialog(Data);
+        }
+
+        public void Show(DialogDef def)
+        {
+            _external = def;
+            Show();
+        }
+
+        public DialogData Data
+        {
+            get
+            {
+                switch (_mode)
+                {
+                    case Mode.Bound:
+                        return _bound;
+                    case Mode.External:
+                        return _external.Data;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public enum Mode
+        {
+            Bound,
+            External
+        }
+    }
+}
