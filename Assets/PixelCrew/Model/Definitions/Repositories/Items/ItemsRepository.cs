61821108b8ea57d580bc676885d69c69267d8573
AlexB ID <butenko.92@mail.ru>
2023-02-17 18:51:56 +0300

    Shop UI is done

--- Assets/PixelCrew/Model/Definitions/Repositories/Items/ItemsRepository.cs
+++ Assets/PixelCrew/Model/Definitions/Repositories/Items/ItemsRepository.cs
@@ -21,10 +21,14 @@ namespace Assets.PixelCrew.Model.Definitions.Repositories.Items
         [SerializeField] private string _id;
         [SerializeField] private Sprite _icon;
         [SerializeField] private ItemTag[] _tags;
-        public string Id => _id;
+        [SerializeField] private string _info;
+        [SerializeField] private ItemWithCount _price;
 
+        public string Id => _id;
         public bool IsVoid => string.IsNullOrEmpty(_id);
         public Sprite Icon => _icon;
+        public string Info => _info;
+        public ItemWithCount Price => _price;
 
         public bool HasTag(ItemTag tag)
         {
