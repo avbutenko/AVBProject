d1bb18917c391c4697de864d3a3055b1aa87f284
avbutenko <butenko.92@mail.ru>
2023-03-28 00:42:36 +0300

    Teleport particles & FlashLightWidjet are done

---
+++ Assets/PixelCrew/UI/Widjets/FlashLightWidjet.cs
@@ -0,0 +1,27 @@
+﻿using Assets.PixelCrew.Model;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.PixelCrew.UI.Widjets
+{
+    public class FlashLightWidjet : MonoBehaviour
+    {
+
+        [SerializeField] private Image _coolDownImage;
+
+        private GameSession _session;
+        private float _capacity;
+
+        private void Start()
+        {
+            _session = GameSession.Instance;
+            _capacity = _session.Data.Fuel.Value;
+        }
+
+        private void Update()
+        {
+            _coolDownImage.fillAmount = 1 - _session.Data.Fuel.Value / _capacity;
+        }
+    }
+}
